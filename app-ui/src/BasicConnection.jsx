5e4095e56ca2824a3621fddab5c09bd245b0904b
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2021-11-15 16:41:00 +0530

    service-integration

--- app-ui/src/BasicConnection.jsx
+++ app-ui/src/BasicConnection.jsx
@@ -27,7 +27,35 @@ const engineTest = () => {
 	let port2 = node2.addPort(new AdvancedPortModel(true, 'in'));
 
 	const model = new DiagramModel();
+	model.registerListener({
+		eventDidFire: async (event) => {
+			const entity = event.entity;
+			const components = entity.getNodes().map((node) => {
+				return {
+					id: node.getOptions().id,
+					name: node.getOptions().name,
+				};
+			});
+			const links = entity.getLinks().map((link) => {
+				return {
+					src: link.sourcePort.parent.options.name,
+					desc: link.targetPort.parent.options.name,
+				};
+			});
 
+			await fetch('/api/state/cache', {
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					components,
+					links,
+				}),
+			});
+		},
+	});
 	const link1 = port1.link(port2);
 	let models = model.addAll(node1, node2, link1);
 
