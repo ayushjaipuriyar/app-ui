ec26460825cea67f21bda92ede13e230b12e4b28
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2021-11-15 16:42:22 +0530

    service-integration
    commit 5e4095e56ca2824a3621fddab5c09bd245b0904b
    Author: Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
    Date:   Mon Nov 15 16:41:00 2021 +0530
    
        service-integration

--- app-ui/server/server.js
+++ app-ui/server/server.js
@@ -1,11 +1,13 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.use(express.json());
+app.use(express.urlencoded());
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+app.post('/api/state/cache', function (req, res) {
+	console.log(req.body);
+	res.status(204).send();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
